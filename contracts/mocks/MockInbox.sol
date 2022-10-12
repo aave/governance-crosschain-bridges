6d4792497abc1dc1db6cfdcfe9723223ba848dcf
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-10-12 13:42:42 +0200

    fix: Add Nitro contracts

--- contracts/mocks/MockInbox.sol
+++ contracts/mocks/MockInbox.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.10;
 
 import {IInbox} from '../dependencies/arbitrum/interfaces/IInbox.sol';
-import {AddressAliasHelper} from '../dependencies/arbitrum/AddressAliasHelper.sol';
+import {IBridge} from '../dependencies/arbitrum/interfaces/IBridge.sol';
+import {ISequencerInbox} from '../dependencies/arbitrum/interfaces/ISequencerInbox.sol';
 
 contract MockInbox is IInbox {
   uint256 public messageNum;
@@ -11,70 +12,106 @@ contract MockInbox is IInbox {
     messageNum = msgNum;
   }
 
-  function sendL2Message(
-    bytes calldata // messageData
-  ) external view override returns (uint256) {
+  function bridge() external view override returns (IBridge) {
+    return IBridge(address(1));
+  }
+
+  function sequencerInbox() external view override returns (ISequencerInbox) {
+    return ISequencerInbox(address(1));
+  }
+
+  function sendL2MessageFromOrigin(bytes calldata messageData) external override returns (uint256) {
     return messageNum;
   }
 
-  function sendUnsignedTransaction(
-    uint256, // maxGas
-    uint256, // gasPriceBid
-    uint256, // nonce
-    address, // destAddr
-    uint256, // amount
-    bytes calldata // data
-  ) external view override returns (uint256) {
-    return messageNum;
-  }
-
-  function sendContractTransaction(
-    uint256, // maxGas
-    uint256, // gasPriceBid
-    address, // destAddr
-    uint256, // amount
-    bytes calldata // data
-  ) external view override returns (uint256) {
+  function sendL2Message(bytes calldata messageData) external override returns (uint256) {
     return messageNum;
   }
 
   function sendL1FundedUnsignedTransaction(
-    uint256, // maxGas
-    uint256, // gasPriceBid
+    uint256, // gasLimit
+    uint256, // maxFeePerGas
     uint256, // nonce
-    address, // destAddr
+    address, // to
     bytes calldata // data
   ) external payable override returns (uint256) {
     return messageNum;
   }
 
   function sendL1FundedContractTransaction(
-    uint256, // maxGas
-    uint256, // gasPriceBid
-    address, // destAddr
+    uint256, // gasLimit
+    uint256, // maxFeePerGas
+    address, // to
     bytes calldata // data
   ) external payable override returns (uint256) {
     return messageNum;
   }
 
-  function createRetryableTicket(
-    address destAddr,
-    uint256, // arbTxCallValue
-    uint256, // maxSubmissionCost
-    address, // submissionRefundAddress
-    address, // valueRefundAddress
-    uint256 maxGas,
-    uint256, // gasPriceBid
-    bytes calldata data
-  ) external payable override returns (uint256) {
-    bool success;
-    (success, ) = destAddr.call{gas: maxGas}(data);
+  function sendUnsignedTransaction(
+    uint256, // gasLimit
+    uint256, // maxFeePerGas
+    uint256, // nonce
+    address, // to
+    uint256, // value
+    bytes calldata // data
+  ) external override returns (uint256) {
     return messageNum;
   }
 
-  function depositEth(
-    uint256 // maxSubmissionCost
-  ) external payable override returns (uint256) {
+  function sendContractTransaction(
+    uint256, // gasLimit
+    uint256, // maxFeePerGas
+    address, // to
+    uint256, // value
+    bytes calldata // data
+  ) external override returns (uint256) {
     return messageNum;
   }
+
+  function calculateRetryableSubmissionFee(uint256 dataLength, uint256 baseFee)
+    external
+    view
+    override
+    returns (uint256)
+  {
+    return 1_000_000;
+  }
+
+  function depositEth() external payable override returns (uint256) {
+    return messageNum;
+  }
+
+  function createRetryableTicket(
+    address to, // to
+    uint256, // l2CallValue
+    uint256, // maxSubmissionCost
+    address, // excessFeeRefundAddress
+    address, // callValueRefundAddress
+    uint256 gasLimit,
+    uint256, // maxFeePerGas
+    bytes calldata data
+  ) external payable override returns (uint256) {
+    bool success;
+    (success, ) = to.call{gas: gasLimit}(data);
+    return messageNum;
+  }
+
+  function unsafeCreateRetryableTicket(
+    address, // to
+    uint256, // l2CallValue
+    uint256, // maxSubmissionCost
+    address, // excessFeeRefundAddress
+    address, // callValueRefundAddress
+    uint256, // gasLimit
+    uint256, // maxFeePerGas
+    bytes calldata // data
+  ) external payable override returns (uint256) {}
+
+  function pause() external override {}
+
+  function unpause() external override {}
+
+  function postUpgradeInit(IBridge _bridge) external override {}
+
+  function initialize(IBridge _bridge, ISequencerInbox _sequencerInbox) external override {}
 }
