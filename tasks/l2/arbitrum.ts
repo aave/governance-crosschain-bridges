c0fb95c87b0eedee6e098d35b5b38e1d6a94eda1
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-10-18 12:27:05 +0200

    feat: Add scripts to manage arbitrum retryable tickets

--- tasks/l2/arbitrum.ts
+++ tasks/l2/arbitrum.ts
@@ -1,6 +1,5 @@
-import { Provider } from '@ethersproject/abstract-provider';
 import { BigNumber, ethers } from 'ethers';
-import { formatUnits, hexDataLength, parseUnits } from 'ethers/lib/utils';
+import { formatUnits, hexDataLength } from 'ethers/lib/utils';
 import { task } from 'hardhat/config';
 import { NETWORKS_RPC_URL } from '../../helper-hardhat-config';
 import { applyL1ToL2Alias } from '../../helpers/arbitrum-helpers';
@@ -13,29 +12,8 @@ import {
   Greeter__factory,
   ArbRetryableTx__factory,
   IInbox__factory,
+  IBridge__factory,
 } from '../../typechain';
-
-const bitFlipSeqNum = (seqNum: BigNumber) => {
-  return seqNum.or(BigNumber.from(1).shl(255));
-};
-
-const computeL2Hash = (chainId: BigNumber, seqNum: BigNumber) => {
-  return ethers.utils.keccak256(
-    ethers.utils.concat([
-      ethers.utils.zeroPad(chainId.toHexString(), 32),
-      ethers.utils.zeroPad(bitFlipSeqNum(seqNum).toHexString(), 32),
-    ])
-  );
-};
-
-const computeRetryableHash = (chainId: BigNumber, seqNum: BigNumber, type: number) => {
-  return ethers.utils.keccak256(
-    ethers.utils.concat([
-      ethers.utils.zeroPad(computeL2Hash(chainId, seqNum), 32),
-      ethers.utils.zeroPad(BigNumber.from(type).toHexString(), 32),
-    ])
-  );
-};
 
 task('arbitrum:proposal-count', '').setAction(async (_, hre) => {
   await hre.run('set-DRE');
@@ -45,9 +23,14 @@ task('arbitrum:proposal-count', '').setAction(async (_, hre) => {
     throw new Error('Missing chain id');
   }
 
+  console.log(DRE.network.name);
+
   if (
-    DRE.network.name != eArbitrumNetwork.arbitrum &&
-    DRE.network.name != eArbitrumNetwork.arbitrumTestnet
+    ![
+      eArbitrumNetwork.arbitrumRinkeby.toString(),
+      eArbitrumNetwork.arbitrumGoerli.toString(),
+      eArbitrumNetwork.arbitrum.toString(),
+    ].includes(DRE.network.name)
   ) {
     throw new Error('Only applicable on arbitrum L2');
   }
@@ -73,16 +56,23 @@ task(
 ).setAction(async (_, hre) => {
   await hre.run('set-DRE');
 
-  if (DRE.network.name != eEthereumNetwork.rinkeby && DRE.network.name != eEthereumNetwork.main) {
+  if (
+    ![
+      eEthereumNetwork.rinkeby.toString(),
+      eEthereumNetwork.goerli.toString(),
+      eEthereumNetwork.main.toString(),
+    ].includes(DRE.network.name)
+  ) {
     throw new Error('Only applicable on mainnet or rinkeby where arbitrum L2 exist');
   }
 
-  const MESSAGE = 'Miguel was also here ;)';
-  const GAS_PRICE_BID = parseUnits('0.5', 9);
+  const MESSAGE = 'RetryableTicket auto-redeem';
 
   let INBOX = ADDRESSES['INBOX_MAIN'];
   if (DRE.network.name == eEthereumNetwork.rinkeby) {
     INBOX = ADDRESSES['INBOX_RINKEBY'];
+  } else if (DRE.network.name == eEthereumNetwork.goerli) {
+    INBOX = ADDRESSES['INBOX_GOERLI'];
   }
 
   const { deployer: deployerAddress } = await DRE.getNamedAccounts();
@@ -101,16 +91,12 @@ task(
   );
   console.log(`Arbitrum Gov at ${arbitrumGov.address}`);
 
+  // Data encoding
   const greeter = Greeter__factory.connect((await l2.deployments.get('Greeter')).address, deployer);
   console.log(`Greeter at ${greeter.address}`);
 
   const inbox = IInbox__factory.connect(INBOX, deployer);
   console.log(`INBOX at: ${inbox.address}`);
-
-  const retryable = ArbRetryableTx__factory.connect(
-    ADDRESSES['RETRYABLE_TICKET_TX_ADDRESS'],
-    l2Provider
-  );
 
   const encodedGreeting = greeter.interface.encodeFunctionData('setMessage', [MESSAGE]);
 
@@ -129,7 +115,8 @@ task(
   ]);
   const bytesLength = hexDataLength(encodedQueue);
 
-  const [submissionCost] = await retryable.getSubmissionPrice(bytesLength);
+  const submissionCost = await inbox.calculateRetryableSubmissionFee(bytesLength, 0);
+  const submissionCostWithMargin = submissionCost.add(ethers.utils.parseUnits('10', 'gwei'));
 
   const gasEstimate = await arbitrumGov.estimateGas.queue(
     targets,
@@ -142,43 +129,185 @@ task(
     }
   );
 
-  const MAX_SUBMISSION_COST = submissionCost.mul(5);
   // Add overhead to cover retryable ticket creation etc
-  const MAX_GAS = BigNumber.from(200000).add(gasEstimate.mul(3).div(2));
-  console.log(`Using max submission cost: ${MAX_SUBMISSION_COST} and max gas: ${MAX_GAS}`);
+  const l2GasLimit = BigNumber.from(200000).add(gasEstimate.mul(3).div(2));
+  const l2GasPrice = ethers.utils.parseUnits('0.4', 'gwei');
+  console.log(`Using max submission cost: ${submissionCostWithMargin} and max gas: ${l2GasLimit}`);
+
+  console.log(
+    'Value needed',
+    formatUnits(submissionCostWithMargin.add(l2GasLimit.mul(l2GasPrice)))
+  );
 
   const tx = await inbox.createRetryableTicket(
-    arbitrumGov.address,
-    0,
-    MAX_SUBMISSION_COST,
-    deployer.address,
-    deployer.address,
-    MAX_GAS,
-    GAS_PRICE_BID,
-    encodedQueue,
+    arbitrumGov.address, // to
+    0, // l2CallValue
+    submissionCostWithMargin, // maxSubmissionCost
+    deployer.address, // excessFeeRefundAddress
+    deployer.address, // callValueRefundAddress
+    l2GasLimit, // gasLimit
+    l2GasPrice, // maxFeePerGas
+    encodedQueue, // data
     {
-      value: MAX_SUBMISSION_COST.add(GAS_PRICE_BID.mul(MAX_GAS)),
+      value: submissionCostWithMargin.add(l2GasLimit.mul(l2GasPrice)),
+    }
+  );
+  await tx.wait();
+
+  console.log(`Transactions: ${tx.hash}`);
+});
+
+task(
+  'arbitrum:initiate-greeting-no-autoredeem',
+  'Queue a greeting in the governance executor on Arbitrum by transacting on L2'
+).setAction(async (_, hre) => {
+  await hre.run('set-DRE');
+
+  if (
+    ![
+      eEthereumNetwork.rinkeby.toString(),
+      eEthereumNetwork.goerli.toString(),
+      eEthereumNetwork.main.toString(),
+    ].includes(DRE.network.name)
+  ) {
+    throw new Error('Only applicable on mainnet, rinkeby or goerli where arbitrum L2 exist');
+  }
+
+  const MESSAGE = 'RetryableTicket no-auto-redeem';
+
+  let INBOX = ADDRESSES['INBOX_MAIN'];
+  if (DRE.network.name == eEthereumNetwork.rinkeby) {
+    INBOX = ADDRESSES['INBOX_RINKEBY'];
+  } else if (DRE.network.name == eEthereumNetwork.goerli) {
+    INBOX = ADDRESSES['INBOX_GOERLI'];
+  }
+
+  const { deployer: deployerAddress } = await DRE.getNamedAccounts();
+  const deployer = await DRE.ethers.getSigner(deployerAddress);
+  console.log(
+    `Deployer address: ${deployer.address} (${formatUnits(await deployer.getBalance())})`
+  );
+
+  const l2 = DRE.companionNetworks['arbitrum'];
+  const l2Name = DRE.network.companionNetworks['arbitrum'];
+  const l2Provider = new ethers.providers.JsonRpcProvider(NETWORKS_RPC_URL[l2Name]);
+
+  const arbitrumGov = ArbitrumBridgeExecutor__factory.connect(
+    (await l2.deployments.get('ArbitrumGov')).address,
+    l2Provider
+  );
+  console.log(`Arbitrum Gov at ${arbitrumGov.address}`);
+
+  // Data encoding
+  const greeter = Greeter__factory.connect((await l2.deployments.get('Greeter')).address, deployer);
+  console.log(`Greeter at ${greeter.address}`);
+
+  const inbox = IInbox__factory.connect(INBOX, deployer);
+  console.log(`INBOX at: ${inbox.address}`);
+
+  const encodedGreeting = greeter.interface.encodeFunctionData('setMessage', [MESSAGE]);
+
+  const targets: string[] = [greeter.address];
+  const values: BigNumber[] = [BigNumber.from(0)];
+  const signatures: string[] = [''];
+  const calldatas: string[] = [encodedGreeting];
+  const withDelegatecalls: boolean[] = [false];
+
+  const encodedQueue = arbitrumGov.interface.encodeFunctionData('queue', [
+    targets,
+    values,
+    signatures,
+    calldatas,
+    withDelegatecalls,
+  ]);
+  const bytesLength = hexDataLength(encodedQueue);
+
+  const submissionCost = await inbox.calculateRetryableSubmissionFee(bytesLength, 0);
+  const submissionCostWithMargin = submissionCost.add(ethers.utils.parseUnits('10', 'gwei'));
+
+  // Add overhead to cover retryable ticket creation etc
+  console.log(`Using max submission cost: ${submissionCostWithMargin}`);
+
+  console.log('Value needed', formatUnits(submissionCostWithMargin));
+
+  const tx = await inbox.createRetryableTicket(
+    arbitrumGov.address, // to
+    0, // l2CallValue
+    submissionCostWithMargin, // maxSubmissionCost
+    deployer.address, // excessFeeRefundAddress
+    deployer.address, // callValueRefundAddress
+    0, // gasLimit
+    0, // maxFeePerGas
+    encodedQueue, // data
+    {
+      value: submissionCostWithMargin,
     }
   );
 
-  console.log(`Transactions initiated: ${tx.hash}`);
+  await tx.wait();
+
+  console.log(`Transaction: ${tx.hash}`);
 });
 
-task('arbitrum:redeem-retryable', 'Redeem a retryable ticket on arbitrum')
-  .addParam('sequenceNumber', 'The sequencer number of the message to redeem')
-  .setAction(async (taskArg, hre) => {
+task('arbitrum:get-ticket-id', 'Redeem a retryable ticket on arbitrum')
+  .addParam('txhash', 'The hash of the L1 transaction')
+  .setAction(async ({ txhash: txHash }, hre) => {
     await hre.run('set-DRE');
 
-    const sequenceNumber = taskArg.sequenceNumber;
-
-    const chainId = DRE.network.config.chainId;
-    if (!chainId) {
-      throw new Error('Missing chain id');
+    if (
+      ![
+        eEthereumNetwork.rinkeby.toString(),
+        eEthereumNetwork.goerli.toString(),
+        eEthereumNetwork.main.toString(),
+      ].includes(DRE.network.name)
+    ) {
+      throw new Error('Only applicable on mainnet, rinkeby or goerli where arbitrum L2 exist');
     }
 
+    const l2Name = DRE.network.companionNetworks['arbitrum'];
+    const l2Provider = new ethers.providers.JsonRpcProvider(NETWORKS_RPC_URL[l2Name]);
+    const chainId = (await l2Provider.getNetwork()).chainId;
+
+    const receipt = await hre.ethers.provider.getTransactionReceipt(txHash);
+    const messages = getL1ToL2Messages(receipt.logs);
+    console.log(`Found ${messages.length} L1ToL2 messages in the transaction`);
+
+    for (const message of messages) {
+      const parsedData = parseInboxMessageDataField(message.inboxMessageDeliveredEvent.data);
+      const ticketId = calculateSubmitRetryableId(
+        chainId,
+        message.messageDeliveredEvent.sender,
+        message.messageDeliveredEvent.messageIndex,
+        message.messageDeliveredEvent.baseFeeL1,
+        parsedData.destAddress,
+        parsedData.l2CallValue,
+        parsedData.l1Value,
+        parsedData.maxSubmissionFee,
+        parsedData.excessFeeRefundAddress,
+        parsedData.callValueRefundAddress,
+        parsedData.gasLimit,
+        parsedData.maxFeePerGas,
+        parsedData.data
+      );
+      console.log(
+        `Message: ${
+          message.messageDeliveredEvent.messageIndex
+        } - RetryableTicketId: ${ticketId.toString()}`
+      );
+    }
+  });
+
+task('arbitrum:redeem-retryable', 'Redeem a retryable ticket on arbitrum')
+  .addParam('ticketid', 'The ticket id to redeem')
+  .setAction(async ({ ticketid: ticketId }, hre) => {
+    await hre.run('set-DRE');
+
     if (
-      DRE.network.name != eArbitrumNetwork.arbitrum &&
-      DRE.network.name != eArbitrumNetwork.arbitrumTestnet
+      ![
+        eArbitrumNetwork.arbitrumRinkeby.toString(),
+        eArbitrumNetwork.arbitrumGoerli.toString(),
+        eArbitrumNetwork.arbitrum.toString(),
+      ].includes(DRE.network.name)
     ) {
       throw new Error('Only applicable on arbitrum L2');
     }
@@ -194,15 +323,216 @@ task('arbitrum:redeem-retryable', 'Redeem a retryable ticket on arbitrum')
       deployer
     );
 
-    const ticketId = computeRetryableHash(
-      BigNumber.from(chainId),
-      BigNumber.from(sequenceNumber),
-      0
+    console.log(`Retryable ticket id: ${ticketId}`);
+    console.log(`Timeout for ticket : ${await retryable.getTimeout(ticketId)} `);
+
+    const tx = await retryable.redeem(ticketId);
+    await tx.wait();
+    console.log(`Ticket redeemed at tx: ${tx.hash}`);
+  });
+
+  task('arbitrum:extend-retryable', 'Extend lifetime of a retryable ticket on arbitrum')
+  .addParam('ticketid', 'The ticket id to redeem')
+  .setAction(async ({ ticketid: ticketId }, hre) => {
+    await hre.run('set-DRE');
+
+    if (
+      ![
+        eArbitrumNetwork.arbitrumRinkeby.toString(),
+        eArbitrumNetwork.arbitrumGoerli.toString(),
+        eArbitrumNetwork.arbitrum.toString(),
+      ].includes(DRE.network.name)
+    ) {
+      throw new Error('Only applicable on arbitrum L2');
+    }
+
+    const { deployer: deployerAddress } = await DRE.getNamedAccounts();
+    const deployer = await DRE.ethers.getSigner(deployerAddress);
+    console.log(
+      `Deployer address: ${deployer.address} (${formatUnits(await deployer.getBalance())})`
+    );
+
+    const retryable = ArbRetryableTx__factory.connect(
+      ADDRESSES['RETRYABLE_TICKET_TX_ADDRESS'],
+      deployer
     );
 
     console.log(`Retryable ticket id: ${ticketId}`);
     console.log(`Timeout for ticket : ${await retryable.getTimeout(ticketId)} `);
 
-    const tx = await retryable.redeem(ticketId);
-    console.log(`Redeeming ticket at tx: ${tx.hash}`);
+    const tx = await retryable.keepalive(ticketId);
+    await tx.wait();
+    console.log(`Ticket lifetime extended at tx: ${tx.hash}`);
+    console.log(`New Timeout for ticket : ${await retryable.getTimeout(ticketId)} `);
   });
+
+task('arbitrum:cancel-retryable', 'Cancel a retryable ticket on arbitrum')
+  .addParam('ticketid', 'The ticket id to redeem')
+  .setAction(async ({ ticketid: ticketId }, hre) => {
+    await hre.run('set-DRE');
+
+    if (
+      ![
+        eArbitrumNetwork.arbitrumRinkeby.toString(),
+        eArbitrumNetwork.arbitrumGoerli.toString(),
+        eArbitrumNetwork.arbitrum.toString(),
+      ].includes(DRE.network.name)
+    ) {
+      throw new Error('Only applicable on arbitrum L2');
+    }
+
+    const { deployer: deployerAddress } = await DRE.getNamedAccounts();
+    const deployer = await DRE.ethers.getSigner(deployerAddress);
+    console.log(
+      `Deployer address: ${deployer.address} (${formatUnits(await deployer.getBalance())})`
+    );
+
+    const retryable = ArbRetryableTx__factory.connect(
+      ADDRESSES['RETRYABLE_TICKET_TX_ADDRESS'],
+      deployer
+    );
+
+    console.log(`Retryable ticket id: ${ticketId}`);
+    console.log(`Timeout for ticket : ${await retryable.getTimeout(ticketId)} `);
+
+    const tx = await retryable.cancel(ticketId);
+    await tx.wait();
+    console.log(`Ticket canceled at tx: ${tx.hash}`);
+  });
+
+const getL1ToL2Messages = (logs: ethers.providers.Log[]) => {
+  const mdEvents = getEvents(
+    IBridge__factory.createInterface(),
+    logs,
+    'MessageDelivered(uint256,bytes32,address,uint8,address,bytes32,uint256,uint64)'
+  );
+  const imdEvents = getEvents(
+    IInbox__factory.createInterface(),
+    logs,
+    'InboxMessageDelivered(uint256,bytes)'
+  );
+
+  const messages: any[] = [];
+  for (const messageDelivered of mdEvents) {
+    const imd = imdEvents.filter((i) => i.messageNum.eq(messageDelivered.messageIndex))[0];
+    if (!imd) {
+      throw new Error(
+        `Unexpected missing event for message index: ${messageDelivered.messageIndex.toString()}. ${JSON.stringify(
+          imdEvents
+        )}`
+      );
+    }
+
+    messages.push({
+      inboxMessageDeliveredEvent: imd,
+      messageDeliveredEvent: messageDelivered,
+    });
+  }
+
+  return messages;
+};
+
+const getEvents = (
+  iface: ethers.utils.Interface,
+  logs: ethers.providers.Log[],
+  eventName: string
+) => {
+  const events: ethers.utils.Result[] = [];
+  logs.map((log) => {
+    // Parse events if it log matches the log topic
+    const event = iface.getEvent(eventName);
+    const topic = iface.getEventTopic(event);
+    if (log.topics[0] == topic) {
+      events.push(iface.parseLog(log).args);
+    }
+  });
+  return events;
+};
+
+const calculateSubmitRetryableId = (
+  l2ChainId: number,
+  fromAddress: string,
+  messageNumber: BigNumber,
+  l1BaseFee: BigNumber,
+  destAddress: string,
+  l2CallValue: BigNumber,
+  l1Value: BigNumber,
+  maxSubmissionFee: BigNumber,
+  excessFeeRefundAddress: string,
+  callValueRefundAddress: string,
+  gasLimit: BigNumber,
+  maxFeePerGas: BigNumber,
+  data: string
+): string => {
+  const formatNumber = (value: BigNumber): Uint8Array => {
+    return ethers.utils.stripZeros(value.toHexString());
+  };
+
+  const chainId = BigNumber.from(l2ChainId);
+  const msgNum = BigNumber.from(messageNumber);
+
+  const fields: any[] = [
+    formatNumber(chainId),
+    ethers.utils.zeroPad(formatNumber(msgNum), 32),
+    fromAddress,
+    formatNumber(l1BaseFee),
+    formatNumber(l1Value),
+    formatNumber(maxFeePerGas),
+    formatNumber(gasLimit),
+    destAddress,
+    formatNumber(l2CallValue),
+    callValueRefundAddress,
+    formatNumber(maxSubmissionFee),
+    excessFeeRefundAddress,
+    data,
+  ];
+
+  // arbitrum submit retry transactions have type 0x69
+  const rlpEnc = ethers.utils.hexConcat(['0x69', ethers.utils.RLP.encode(fields)]);
+
+  return ethers.utils.keccak256(rlpEnc);
+};
+
+const parseInboxMessageDataField = (eventData: string) => {
+  // decode the data field - is been packed so we cant decode the bytes field this way
+  const parsed = ethers.utils.defaultAbiCoder.decode(
+    [
+      'uint256', // dest
+      'uint256', // l2 call value
+      'uint256', // msg val
+      'uint256', // max submission
+      'uint256', // excess fee refund addr
+      'uint256', // call value refund addr
+      'uint256', // max gas
+      'uint256', // gas price bid
+      'uint256', // data length
+    ],
+    eventData
+  ) as BigNumber[];
+
+  const addressFromBigNumber = (bn: BigNumber) =>
+    ethers.utils.getAddress(ethers.utils.hexZeroPad(bn.toHexString(), 20));
+
+  const destAddress = addressFromBigNumber(parsed[0]);
+  const l2CallValue = parsed[1];
+  const l1Value = parsed[2];
+  const maxSubmissionFee = parsed[3];
+  const excessFeeRefundAddress = addressFromBigNumber(parsed[4]);
+  const callValueRefundAddress = addressFromBigNumber(parsed[5]);
+  const gasLimit = parsed[6];
+  const maxFeePerGas = parsed[7];
+  const callDataLength = parsed[8];
+  const data = '0x' + eventData.substring(eventData.length - callDataLength.mul(2).toNumber());
+
+  return {
+    destAddress,
+    l2CallValue,
+    l1Value,
+    maxSubmissionFee: maxSubmissionFee,
+    excessFeeRefundAddress,
+    callValueRefundAddress,
+    gasLimit,
+    maxFeePerGas,
+    data,
+  };
+};
